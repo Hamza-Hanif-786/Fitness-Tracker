70612a1991b6dd542e09620e8a9d3938094bca17
Hamza Hanif <hamzahanif1024@gmail.com>
2024-06-12 11:15:02 +0500

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import 'primereact/resources/themes/lara-light-blue/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css'; // flex
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
